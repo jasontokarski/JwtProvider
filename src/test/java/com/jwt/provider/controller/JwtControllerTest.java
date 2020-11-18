f3173cd872f84f05bf11b25ab7f0edd7fb86499e
jasontokarski <z1805312@students.niu.edu>
2020-11-17 19:54:16 -0600

    Updated JUnits to use H2 and added Dockerfiler

--- src/test/java/com/jwt/provider/controller/JwtControllerTest.java
+++ src/test/java/com/jwt/provider/controller/JwtControllerTest.java
@@ -6,6 +6,8 @@ import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
 
+import javax.sql.DataSource;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -18,16 +20,21 @@ import com.jwt.provider.service.JwtService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest(properties = "spring.main.allow-bean-definition-overriding=true")
 @AutoConfigureMockMvc 
+@TestPropertySource(value="classpath:/application-junit.properties")
 public class JwtControllerTest {
     
     @Autowired
@@ -51,6 +58,22 @@ public class JwtControllerTest {
         public Clock clock() { 
             return Clock.fixed(Instant.parse("2020-10-26T00:00:00Z"), ZoneId.of("UTC"));
         } 
+
+        @Bean
+        @Primary
+        DataSource createDataSource(){
+            return DataSourceBuilder.create()
+            .driverClassName("org.h2.Driver")
+            .url("jdbc:h2:mem:jwt_provider")
+            .username("SA")
+            .password("")
+            .build();
+        }
+
+        @Bean
+        public static PropertySourcesPlaceholderConfigurer propertiesResolver() {
+            return new PropertySourcesPlaceholderConfigurer();
+        }
     }
 
     @BeforeEach
@@ -60,6 +83,7 @@ public class JwtControllerTest {
         }
     }
 
+    
     @Test
 	public void contextLoads() throws Exception {
         assertThat(jwtController).isNotNull();
@@ -67,16 +91,6 @@ public class JwtControllerTest {
         assertThat(jwtRepository).isNotNull();
         assertThat(objectMapper).isNotNull();
         assertThat(jwtService).isNotNull();
-    }
-    
-    @Test
-    public void retrieveJwtValidUsernameAndApiKey() throws Exception {
-        String expectedJwtToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0dXNlcjEyMyIsImlhdCI6MTYwMzY3MDQwMCwiaXNzIjoiWVd4cWEzTmtabXBzWVhOa2EyRXdhbVE1WmpCcVlXWXdPV293Wm1Fd09XcG1NRGxxTUdGaCIsImV4cCI6MTYwMzc1NjgwMH0.nZ06Z-VdQ7lryfKF6mDJVK_1mjs0MmpnSvBBLWTdfAQ";
-        MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.post("/jwt/request").contentType("application/json").accept("application/json").content(objectMapper.writeValueAsString(new AuthEntity("testuser123", "apikey408374"))))
-            .andExpect(status().isOk()).andReturn();
-        String jwtToken = mvcResult.getResponse().getContentAsString();
-        TokenEntity tokenEntity = new ObjectMapper().readValue(jwtToken, TokenEntity.class);
-        assertThat(tokenEntity.getToken()).isEqualTo(expectedJwtToken);
     }
 
     @Test
@@ -101,6 +115,17 @@ public class JwtControllerTest {
     public void retrieveJwtMissingApiKey() throws Exception {
         this.mockMvc.perform(MockMvcRequestBuilders.post("/jwt/request").contentType("application/json").accept("application/json").content("{\"user\":\"testuser123\"}"))
         .andExpect(status().isUnauthorized());
+    }
+    
+    @Test
+    public void retrieveJwtValidUsernameAndApiKey() throws Exception {
+        jwtService.setClock(Clock.fixed(Instant.parse("2020-10-26T00:00:00Z"), ZoneId.of("UTC")));
+        String expectedJwtToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0dXNlcjEyMyIsImlhdCI6MTYwMzY3MDQwMCwiaXNzIjoiWVd4cWEzTmtabXBzWVhOa2EyRXdhbVE1WmpCcVlXWXdPV293Wm1Fd09XcG1NRGxxTUdGaCIsImV4cCI6MTYwMzc1NjgwMH0.nZ06Z-VdQ7lryfKF6mDJVK_1mjs0MmpnSvBBLWTdfAQ";
+        MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.post("/jwt/request").contentType("application/json").accept("application/json").content(objectMapper.writeValueAsString(new AuthEntity("testuser123", "apikey408374"))))
+            .andExpect(status().isOk()).andReturn();
+        String jwtToken = mvcResult.getResponse().getContentAsString();
+        TokenEntity tokenEntity = new ObjectMapper().readValue(jwtToken, TokenEntity.class);
+        assertThat(tokenEntity.getToken()).isEqualTo(expectedJwtToken);
     }
 
     @Test
@@ -134,9 +159,7 @@ public class JwtControllerTest {
 
     @Test
     public void expiredJwt() throws Exception {
-        String expiredJwtToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0dXNlcjEyMyIsImlhdCI6MTYwMzY3MDQwMCwiaXNzIjoiWVd4cWEzTmtabXBzWVhOa2EyRXdhbVE2WmpCcVlXWXdPV293Wm1Fd09XcG1NRGxxTUdGaCIsImV4cCI6MTYwMzc1NjgwMH0.nZ06Z-VdQ7lryfKF6mDJVK_1mjs0MmpnSvBBLWTdfAQ";
-        //this.mockMvc.perform(MockMvcRequestBuilders.post("/jwt/request").contentType("application/json").accept("application/json").content(objectMapper.writeValueAsString(new AuthEntity("testuser123", "apikey408374"))))
-            //.andExpect(status().isOk());
+        String expiredJwtToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0dXNlcjEyMyIsImlhdCI6MTYwMzY3MDQwMCwiaXNzIjoiWVd4cWEzTmtabXBzWVhOa2EyRXdhbVE2WmpCcVlXWXdPV293Wm1Fd09XcG1NRGxxTUdGaCIsImV4cCI6MTYwMzc1NjgwMH0=.nZ06Z-VdQ7lryfKF6mDJVK_1mjs0MmpnSvBBLWTdfAQ";
         assertEquals(false, jwtService.isValidToken(expiredJwtToken));
     }
 }
